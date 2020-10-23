b511e18eab9f817a0300acb127ac5c5f2739d45f
jordanst3wart <jordanstewart2428@gmail.com>
2020-10-23 19:37:45 +1100

    add comments

--- cli-examples.sh
+++ cli-examples.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# breakdown/deconstruct files
 ./index.js  --breakdownCollection --inputDir resources/sample-input  --outputDir resources/sample-output
 
+# reconstruct/construct files
 ./index.js  --reconstructCollection --outputDir resources/sample-input  --inputDir resources/sample-output
 
+# assert the files are the same TODO
+./index.js --assert
